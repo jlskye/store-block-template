add9640f64b1cf764e54468846652543fb2e9022
lan.jiang <lan.jiang@thoughtworks.com>
2023-08-21 16:16:56 +0800

    modify countdown component

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,64 +1,66 @@
-import React, { useState } from 'react'
-import { useCssHandles } from 'vtex.css-handles'
-import { useQuery } from 'react-apollo'
+import React, { useState } from "react";
+import { useCssHandles } from "vtex.css-handles";
+import { useProduct } from "vtex.product-context";
+import { useQuery } from "react-apollo";
 
-import { getTwoDaysFromNow, tick } from './utils/time'
-import type { TimeSplit } from './typings/global'
-import productReleaseDate from './graphql/productReleaseDate.graphql'
+import { getTwoDaysFromNow, tick } from "./utils/time";
+import type { TimeSplit } from "./typings/global";
+import productReleaseDate from "./graphql/productReleaseDate.graphql";
 
-const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
 
-const CSS_HANDLES = ['countdown']
+const CSS_HANDLES = ["countdown"];
 
 const Countdown: StorefrontFunctionComponent = () => {
-  const { data, loading, error } = useQuery(productReleaseDate, {
-    ssr: false,
-  })
+  const productContextValue = useProduct();
+  const { data, loading, error } = useQuery(productReleaseDate);
 
   const [timeRemaining, setTime] = useState<TimeSplit>({
-    hours: '00',
-    minutes: '00',
-    seconds: '00',
-  })
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  });
 
-  const handles = useCssHandles(CSS_HANDLES)
+  const handles = useCssHandles(CSS_HANDLES);
 
-  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+  tick(DEFAULT_TARGET_DATE, setTime);
   if (loading) {
     return (
       <div>
         <span>Loading...</span>
       </div>
-    )
+    );
   }
 
   if (error) {
     return (
       <div>
-        <span>Error!</span>
+        <span>Error</span>
       </div>
-    )
+    );
   }
 
   return (
     <div className={`${handles.countdown} db tc`}>
+      <div>{productContextValue?.product?.productName}</div>
+      {data.category.id}
       {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
     </div>
-  )
-}
+  );
+};
 
 Countdown.schema = {
-  title: 'editor.countdown.title',
-  description: 'editor.countdown.description',
-  type: 'object',
+  title: "editor.countdown.title",
+  description: "editor.countdown.description",
+  type: "object",
   properties: {
     targetDate: {
-      title: 'Final date',
-      description: 'Final date used in the countdown',
-      type: 'string',
+      title: "Final date",
+      description: "Final date used in the countdown",
+      type: "string",
       default: null,
     },
   },
-}
+};
 
-export default Countdown
+export default Countdown;
